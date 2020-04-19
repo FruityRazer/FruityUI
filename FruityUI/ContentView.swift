490e1bb22e437fcf47cb0edc9f7ac30d3c03962e
Eduardo Almeida <me@edr.io>
2020-04-19 17:10:34 +0100

    Initial Commit

---
+++ FruityUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FruityUI
+//
+//  Created by Eduardo Almeida on 19/04/2020.
+//  Copyright © 2020 Eduardo Almeida. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
